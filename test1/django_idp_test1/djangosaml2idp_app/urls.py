6ba21920d94a6c872ebd69c8b5cd98ec69ad8216
Pmeet-searce <meet.parekh@searce.com>
2020-03-19 11:29:04 +0530

    mapped views to urls.py file

--- test1/django_idp_test1/djangosaml2idp_app/urls.py
+++ test1/django_idp_test1/djangosaml2idp_app/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
+from . import views
+
+app_name = 'djangosaml2idp_app' 
 
 urlpatterns = [
+    path('sso/init/', views.SSOInitView.as_view(), name="saml_idp_init"),
+    path('sso/<str:binding>/',views.sso_entry, name="saml_login_binding"),
+    path('login/process/', views.LoginProcessView.as_view(), name="saml_login_process"),
+    path('slo/<str:binding>/', views.LogoutProcessView.as_view(), name="saml_logout_binding"),
+    path('metadata/',views.metadata, name='saml2_idp_metadata'),
     
 ]
